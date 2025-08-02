6fc28aa758f536a8a6d378e59ec5fbf9baea2dcd
Oleg Viatkin <olegviatkin@berkeley.edu>
2025-08-02 15:33:46 -0700

    errors

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,10 +1,10 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+import { type ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+export const createClient = (cookieStore: ReadonlyRequestCookies) => {
   return createServerClient(
     supabaseUrl!,
     supabaseKey!,
